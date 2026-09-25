bcebed9129d51fa037996eedaf97d4480b562cc2
kayknz <147441776+kayknz@users.noreply.github.com>
2026-09-25 09:09:46 +0300

    Set relative base in vite.config.ts for Capacitor iOS asset resolution

--- thk-mobile/vite.config.ts
+++ thk-mobile/vite.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'node:url';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   build: {
     outDir: 'www',
     emptyOutDir: true,
---
+++ thk-mobile/www/assets/index-CY-HZEn3.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-C3LKLTWp.js","./index-Qj0Ytlcv.js","./index-Xd4KjmEr.css"])))=>i.map(i=>d[i]);
+import{r,_ as p}from"./index-Qj0Ytlcv.js";const o=r("App",{web:()=>p(()=>import("./web-C3LKLTWp.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.AppWeb)});export{o as App};
